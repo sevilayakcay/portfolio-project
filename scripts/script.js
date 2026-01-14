57b5e2f45eeba2dbd76a96d29749f5169f41a1ce
Sevilay Guns <95164175+sevilayakcay@users.noreply.github.com>
2026-01-14 21:13:34 +0100

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const toggleBtn = document.querySelector(".hamburger");
+const dropDown = document.querySelector(".drop-down");
+
+toggleBtn.addEventListener("click", e => {
+  e.stopPropagation();
+  dropDown.classList.toggle("active");
+});
+document.addEventListener("click", () => {
+  dropDown.classList.remove("active");
+});
+window.addEventListener("resize", () => {
+  if (window.innerWidth >= 768) {
+    dropDown.classList.remove("active");
+  }
+});
+const images = ["images/banner_1.jpg", "images/banner-5.jpg"];
+
+const bannerDiv = document.querySelector(".banner");
+let currentIndex = 0;
+
+const slideBanner = () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  bannerDiv.style.backgroundImage = `url(${images[currentIndex]})`;
+};
+
+setInterval(slideBanner, 3000);
